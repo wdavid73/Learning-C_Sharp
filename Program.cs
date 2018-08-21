2e504a0ce6c5980f2b1d7f165be45ab01d483514
Wilson Padilla <wilsonpadilla2014@gmail.com>
2018-08-20 20:11:56 -0500

    Primer Hola Mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AprendiendoC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
